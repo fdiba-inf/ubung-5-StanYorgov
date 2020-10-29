b56e9c580b35ad7010de64c8a7fff77e11061cb9
StanYorgov <stanimir_yorgov@yahoo.com>
2020-10-29 09:28:39 +0200

    Übung 5 code optimization

--- src/exercise5/Search.java
+++ src/exercise5/Search.java
@@ -24,6 +24,7 @@ public class Search {
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == number) {
                 numberIndex = i;
+                break;
             }
         }
 
--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -18,26 +18,16 @@ public class UniqueNumbers {
         }
 
         int[] uniqueNumbers = new int[numbers.length];
-        int uniqueNumbersTemp;
 
-        for (int index = 0; index < numbers.length - 1; index++) {
-            for (int k = index + 1; k < numbers.length; k++) {
-                if (numbers[index] == numbers[k]) {
-                    numbers[k] = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (uniqueNumbers[j] == 0) {
+                    uniqueNumbers[j] = numbers[i];
+                    break;
+                } else if (uniqueNumbers[j] == numbers[i]) {
+                    break;
                 }
             }
-        }
-
-        for (int index = 0; index < numbers.length; index++) {
-            for (int j = index + 1; j < numbers.length; j++) {
-                if (numbers[index] == 0) {
-                    uniqueNumbersTemp = numbers[j];
-                    numbers[index] = uniqueNumbersTemp;
-                    numbers[j] = 0;
-                }
-            }
-
-            uniqueNumbers[index] = numbers[index];
         }
 
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
